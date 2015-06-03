ee8fb40c5754e22ca43f1c466bd86c4b24c7b7bf
jeffwhaley <jeffwhaley@gmail.com>
2015-06-03 12:04:21 -0500

    Setup web server and add a route that returns Merry Christmas

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn textsculpture:app --log-file=-
--- textsculpture.py
+++ textsculpture.py
@@ -1,1 +1,14 @@
 import os
+from flask import Flask, Response, request
+import requests
+import tinycss2
+from tinycss2 import color3
+
+app = Flask(__name__)
+@app.route('/',methods = ['GET'])
+def merry_christmas():
+	return 'Merry Christmas!'
+	
+if __name__ == '__main__':
+	app.run()
+
