f2d1da00b659f06443e2da63117a081d279cd152
jeffwhaley <jeffwhaley@gmail.com>
2015-06-03 12:44:44 -0500

    replace textsculpture.py with one from Will's Git archive

--- textsculpture.py
+++ textsculpture.py
@@ -1,14 +1,11 @@
 import os
-from flask import Flask, Response, request
 import requests
 import tinycss2
 from tinycss2 import color3
+from flask import Flask, Response, request
 
 app = Flask(__name__)
-@app.route('/',methods = ['GET'])
-def merry_christmas():
-	return 'Merry Christmas!'
-	
-if __name__ == '__main__':
-	app.run()
 
+@app.route('/', methods=['GET'])
+def merry_christmas():
+  return 'Merry Christmas!'
