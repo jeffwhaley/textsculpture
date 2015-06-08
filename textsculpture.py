5c039e059ff2534069205db43699938e6477892b
jeffwhaley <jeffwhaley@gmail.com>
2015-06-08 18:30:59 -0500

    connected to twilio

--- textsculpture.py
+++ textsculpture.py
@@ -1,7 +1,8 @@
 import os
 import requests
+# import venv.lib.python2\.7.site-packages.requests.pip._vendor.requests
 import tinycss2
-from tinycss2 import color3
+# from tinycss2 import color3
 from flask import Flask, Response, request
 
 app = Flask(__name__)
@@ -9,3 +10,13 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def merry_christmas():
   return 'Merry Christmas!'
+  
+@app.route('/sms', methods=['POST'])
+def sms():
+	body = request.values.get('Body', None)
+	sms = body.lower()
+	print sms
+
+if __name__ == '__main__':
+	app.run
+	
