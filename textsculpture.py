4c4fe7efb1b8eb931af632ac92c35dd887b0ff0d
jeffwhaley <jeffwhaley@gmail.com>
2015-06-10 12:39:28 -0500

    adding debug

--- textsculpture.py
+++ textsculpture.py
@@ -15,17 +15,18 @@ def sms():
 	sms = body.lower()
 	print sms
 	rgba = tinycss2.color3.parse_color(sms)
+	print 'len rgba=' + len(rgba)
 	
 	if rgba is None:
-		print "Not a valid color"
-		# could also send a return SMS here
+		print 'Not a valid color'
+		return Response('Sorry, I don't recognize this color',  mimetype='text/plain')
 		
 	if len(rgba) == 4:
 		red = int(round(255*rgba[0]))
 		green = int(round(255*rgba[1]))
 		blue = int(round(255*rgba[2]))
 		rgba_string = '[{0:03d},{1:03d},{2:03d}]'.format(red, green, blue)
-		print "color" + rgba_string
+		print 'color' + rgba_string
 
 if __name__ == '__main__':
 	app.run
